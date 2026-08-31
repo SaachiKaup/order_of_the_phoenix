3027a19fe1c7cbff6d2f9c536d67e6ecac6c260c
Saachi Kaup <saachi.kaup@gmail.com>
2026-08-31 16:31:57 +0530

    feat - added sg config for ALB and RDS

--- infra/main.tf
+++ infra/main.tf
@@ -44,3 +44,52 @@ resource "aws_security_group_rule" "app_ec2_egress_all" {
   to_port           = 0
   protocol          = "-1"
 }
+
+resource "aws_security_group" "app_alb_sg" {
+  name        = "${local.name}-alb-sg"
+  description = "Security group for the application load balancer"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "app_alb_ingress_http" {
+  type              = "ingress"
+  security_group_id = aws_security_group.app_alb_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+}
+
+resource "aws_security_group_rule" "app_alb_ingress_https" {
+  type              = "ingress"
+  security_group_id = aws_security_group.app_alb_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+}
+
+resource "aws_security_group_rule" "app_alb_egress_ec2" {
+  type                     = "egress"
+  security_group_id        = aws_security_group.app_alb_sg.id
+  source_security_group_id = aws_security_group.app_ec2_sg.id
+  from_port                = var.app_port
+  to_port                  = var.app_port
+  protocol                 = "tcp"
+}
+
+resource "aws_security_group" "app_rds_sg" {
+  name        = "${local.name}-rds-sg"
+  description = "Security group for the RDS database"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "app_rds_ingress_postgres" {
+  type                     = "ingress"
+  security_group_id        = aws_security_group.app_rds_sg.id
+  source_security_group_id = aws_security_group.app_ec2_sg.id
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+}
+
