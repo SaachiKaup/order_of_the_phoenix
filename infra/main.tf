27857caa4844640558e51fad146170f5b4b4a2e1
Saachi Kaup <saachi.kaup@gmail.com>
2026-08-31 16:24:30 +0530

    fix - fixed ids of  security groups mentioned

--- infra/main.tf
+++ infra/main.tf
@@ -29,8 +29,8 @@ resource "aws_security_group" "app_ec2_sg" {
 
 resource "aws_security_group_rule" "app_ec2_from_alb" {
   type                     = "ingress"
-  security_group_id        = aws_security_group.ec2.id
-  source_security_group_id = aws_security_group.alb.id
+  security_group_id        = aws_security_group.app_ec2_sg.id
+  source_security_group_id = aws_security_group.app_alb_sg.id
   from_port                = var.app_port
   to_port                  = var.app_port
   protocol                 = "tcp"
@@ -38,7 +38,7 @@ resource "aws_security_group_rule" "app_ec2_from_alb" {
 
 resource "aws_security_group_rule" "app_ec2_egress_all" {
   type              = "egress"
-  security_group_id = aws_security_group.ec2.id
+  security_group_id = aws_security_group.app_ec2_sg.id
   cidr_blocks       = ["0.0.0.0/0"]
   from_port         = 0
   to_port           = 0
