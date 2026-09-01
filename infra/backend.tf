f75c1bbca2db6edbac443bd69158a1b00b9ff59b
Saachi Kaup <saachi.kaup@gmail.com>
2026-09-01 12:52:39 +0530

    configured with existing dyanamodb locks

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "order-of-the-phoenix-tf-state"
-    key          = "order-of-the-phoenix/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
+    bucket         = "order-of-the-phoenix-tf-state"
+    key            = "order-of-the-phoenix/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "order-of-the-phoenix-tf-locks"
   }
 }
